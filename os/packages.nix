3006e860a9c1d20d6557371064078aec37c2365b
Tomkoid <tomkoid@tomkoid.tk>
2023-10-31 17:56:35 +0100

    use eza instead of exa
    Signed-off-by: Tomkoid <tomkoid@tomkoid.tk>

--- os/packages.nix
+++ os/packages.nix
@@ -9,7 +9,7 @@ in
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     unstable.neovim
-    exa
+    eza
     delta
     bat
     procs
