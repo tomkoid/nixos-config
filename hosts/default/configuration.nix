f094fc7c2ec4b69fb0beeb19f350849533e7d87f
Tomkoid <tomaszierl@outlook.com>
2024-02-23 23:33:08 +0100

    add cachix

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -108,9 +108,15 @@
     settings = {
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
-      substituters = ["https://hyprland.cachix.org"];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
     gc = {
