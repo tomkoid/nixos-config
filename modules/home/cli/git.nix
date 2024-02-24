13bea1c3134c8cde593919d616efd6aab250d3d1
Tomkoid <tomaszierl@outlook.com>
2024-02-24 20:05:37 +0100

    lazygit: use delta and sign commits
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- modules/home/cli/git.nix
+++ modules/home/cli/git.nix
@@ -19,8 +19,22 @@
       };
     };
 
-    home.packages = with pkgs; [
-      lazygit
-    ];
+    # lazygit config
+    programs.lazygit = {
+      enable = true;
+      package = pkgs.lazygit;
+      settings = {
+        overrideGpg = true;
+        git = {
+          commit = {
+            signOff = true;
+          };
+          paging = {
+            colorArg = "never";
+            pager = "delta --dark --paging=never";
+          };
+        };
+      };
+    };
   };
 }
