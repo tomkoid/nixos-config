2abae365f8d3ea7d9cc63e7bf70585a6f7737fa6
Tomkoid <tomaszierl@outlook.com>
2024-03-08 14:35:56 +0100

    hardware: install some intel tools
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- hosts/default/hardware-configuration.nix
+++ hosts/default/hardware-configuration.nix
@@ -20,8 +20,18 @@
     enable = true;
     driSupport32Bit = true;
     driSupport = true;
-    extraPackages = [ pkgs.mesa.drivers ];
+    extraPackages = with pkgs; [
+      pkgs.mesa.drivers
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
   };
+
+  environment.systemPackages = [
+    pkgs.intel-gpu-tools
+  ];
 
   fileSystems."/" =
     {
