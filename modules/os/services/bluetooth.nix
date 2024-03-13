f6b251821acf17b3678aec098e456a2b6594b2bf
Tomkoid <tomaszierl@outlook.com>
2024-03-13 21:40:22 +0100

    os(bluetooth): add mpris proxy
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- modules/os/services/bluetooth.nix
+++ modules/os/services/bluetooth.nix
@@ -1,6 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
 }
