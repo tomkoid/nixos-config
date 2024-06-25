558530e1e37bf215edc713a0ab131a8ed5dabbbe
Tomkoid <tomaszierl@outlook.com>
2024-06-26 00:39:17 +0200

    os(packages): add alacritty
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- modules/os/packages.nix
+++ modules/os/packages.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim
+    alacritty
     kitty
     wezterm
     zsh
