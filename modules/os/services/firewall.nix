a72bbe39414760760dd28460f91dd903692ee1c3
Tomkoid <tomaszierl@outlook.com>
2024-07-02 12:00:55 +0200

    firewall: allow port for chromecast
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- modules/os/services/firewall.nix
+++ modules/os/services/firewall.nix
@@ -4,7 +4,7 @@
   networking.firewall = {
     enable = true;
 
-    allowedTCPPorts = [ 51413 8080 8384 6969 3000 ];
+    allowedTCPPorts = [ 51413 8010 8080 8384 6969 3000 ];
     allowedUDPPorts = [ 51413 ];
   };
 }
